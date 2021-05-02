3b0705991bbc70094d6edbcbb5b4f31738959087
georgetonjr <37754623+georgetonjr@users.noreply.github.com>
2021-05-02 16:37:59 -0300

    finalizando js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const dino = document.querySelector(".dino");
 const background = document.querySelector(".background");
 
 let isJumping = false;
+let position = 0;
 
 function handleKeyUp(event) {
   if (event.keyCode === 32 || event.keyCode === 38) {
@@ -12,7 +13,6 @@ function handleKeyUp(event) {
 }
 
 function jump() {
-  let position = 0;
   isJumping = true;
   let upInterval = setInterval(() => {
     if (position >= 150) {
@@ -52,6 +52,9 @@ function createCactus() {
     if (cactusPosition < -60) {
       clearInterval(leftInterval);
       background.removeChild(cactus);
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+      clearInterval(leftInterval);
+      document.body.innerHTML = "<h1 class='game-over'>Fim de jogo!!</h1>";
     } else {
       cactusPosition -= 10;
       cactus.style.left = cactusPosition + "px";
